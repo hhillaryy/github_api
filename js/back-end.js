42a5214324473d851289caca7d36e5af2c6cbf7a
Hillary Hintzen <hillary07@gmail.com>
2016-03-18 10:45:16 -0700

    tried to write code to test api but getting rate limit error, not sure if it works

--- js/back-end.js
+++ js/back-end.js
@@ -1,6 +1,6 @@
 
 exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,1 +1,25 @@
 var apiKey = require('./../.env').apiKey;
+
+
+$(document).ready(function() {
+ // event.preventDefault();
+$('#submit').click(function(){
+  var user =
+  // $('#bikeLoc').val('');
+  // $('#bikeColor').val('');
+    $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+      console.log(response.public_repos);
+    $('#results').append("<h3>The user " + user + " 's repositories: b</h3>");
+
+    response.users.forEach(function(user){
+      users.push(new User(user.public_repos, user.description,
+        // need to find these json file name things somewhere for github
+    });
+
+    // bikes.forEach(function(bike) {
+    //   $('#results').append("<li>" + "Manufacturer: " + bikeToDisplay.manufacturer + ", " + "model: " + bikeToDisplay.model + ", " + bikeToDisplay.bikeLocation + ", " + "serial number: " + bikeToDisplay.serial + "</li>")
+    // });
+
+  });
+  });
+});
